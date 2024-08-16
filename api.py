ad1e7890d06f9466cc30d28913b1df8795f2458b
yayadiallo <yayadiallo2012dev@gmail.com>
2024-08-16 18:03:23 +0000

    apllication pdf et api

---
+++ api.py
@@ -0,0 +1,12 @@
+import requests 
+
+api_key = "fcf651a2d8cf40ff8ff69f16b109bd31"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-16&sortBy=publishedAt&apiKey=fcf651a2d8cf40ff8ff69f16b109bd31"
+
+request = requests.get(url)
+content = request.json()
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
+    print(article['author'])
+
